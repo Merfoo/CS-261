333f12b8f95105f92c16eea52415fcdacdb8651c
Merfoo <fauzimerfoo@gmail.com>
2016-03-31 19:34:10 -0700

    Added test assignment

---
+++ Homeworks/Test/PrintPrime.c
@@ -0,0 +1,41 @@
+/*
+    Name: Fauzi Kliman    Date: 3/31/16
+    Assignment: Test assignment
+    Development Env: vim
+*/
+
+#include<stdio.h>
+
+/*
+  Description: Checks if an integer is a prime number
+  Input: x, the integer to check if its prime
+  Output: 1 if x is a prime number, 0 otherwise
+*/
+int isPrime(int x)
+{
+    if(x < 2)
+        return 0;
+
+    int i = 2;
+
+    for(i = 2; i <= x / 2; i++)
+        if(x % i == 0)
+            return 0;
+
+    return 1;
+}
+
+int main()
+{
+    int n = 0;
+
+    while(1)
+    {
+        if(isPrime(n))
+            printf("%d\n", n);
+        
+        n++;
+    }
+
+    return 0;
+}
